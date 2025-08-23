1356d96dc67e94f808ac80e1a4d604d82b144a7b
Nils Juto <nils.nilssen9@gmail.com>
2025-08-23 19:37:52 +0200

    Fix langref command to new zig env that outputs zon

--- .config/fish/functions/langref.fish
+++ .config/fish/functions/langref.fish
@@ -1,5 +1,4 @@
 function langref
-    set std_dir (zig env 2> /dev/null | jq '."lib_dir"' | tr -d '"')
-    set langref_file $std_dir/../doc/langref.html
-    open $langref_file
+    set dir (zig any list-installed | grep (zig version) | sed 's/^[^ ]*\t//' )
+    open $dir/doc/langref.html
 end
